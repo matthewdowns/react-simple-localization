de25aafc066762a8f8b611a8f673f3ff66bd9116
Matthew Downs <matthew.downsc@gmail.com>
2020-08-28 15:24:33 -0500

    export LanguageSelect

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { default as LanguageSelect } from './components/LanguageSelect';
 export { default as Translate } from './components/Translate';
 
 export { default as LocalizationContext } from './context/LocalizationContext';
