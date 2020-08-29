7cd7c51c2124dfc317443dc8ae10026a148262c9
Matthew Downs <matthew.downsc@gmail.com>
2020-08-29 11:26:07 -0500

    remove invalid export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { default as LanguageSelect } from './components/LanguageSelect';
 export { default as Translate } from './components/Translate';
 
 export { default as LocalizationContext } from './context/LocalizationContext';
