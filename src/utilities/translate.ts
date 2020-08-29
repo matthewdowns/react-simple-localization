5c057e54e7deb2145fc01e72cbd269f64996331d
Matthew Downs <matthew.downsc@gmail.com>
2020-08-29 15:03:24 -0500

    allow function params to be null

--- src/utilities/translate.ts
+++ src/utilities/translate.ts
@@ -1,10 +1,12 @@
 import Languages from '../types/Languages';
 
-const translate = (language: Languages, translations: { [shortcode: string]: string }) => {
-    for (let shortcode in Languages) {
-        //@ts-ignore
-        if (Languages[shortcode] === language) {
-            return translations[shortcode];
+const translate = (language?: Languages, translations?: { [shortcode: string]: string }) => {
+    if (language && translations) {
+        for (let shortcode in Languages) {
+            //@ts-ignore
+            if (Languages[shortcode] === language) {
+                return translations[shortcode];
+            }
         }
     }
 };
